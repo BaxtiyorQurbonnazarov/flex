ee712716903f9886b953dbcc6a51a7e1c7e55e64
BaxtiyorQurbonnazarov <baxtiyorqurbonnazarov348@gmail.com>
2022-06-23 10:11:36 +0500

    add new file

---
+++ javascript/newFile.js
@@ -0,0 +1,106 @@
+// // CA / SJGDJO;
+// // LSND.KVJKDXZ;
+// // <input type="number">
+// // <form name="myform" action="#" onsubmit="return validate()">number:<input type="text" name="num"><br><span id="demo"></span>
+// // </br></form>
+// // <input type="submit" value="submit"></input>
+// // // let misol = +prompt('birinchi raqamni kiriting')
+// // // let number = +prompt ('ikkinchi raqamni kiriting')
+// // // let amal = prompt('amalni kiriting')
+// // // let qiymat = alert("natijani ko'rish uchun (ok) bosing")
+
+// // // alert(number * misol);
+// // // function numbers(){
+// // //     alert("first.addEventListener")
+// // // }
+
+
+
+
+// <!DOCTYPE html>
+// <html lang="en">    
+// <head>
+//     <meta charset="UTF-8">
+//     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <link rel="stylesheet" href="style.css">
+//     <title>Java scripti darslar</title>
+// </head>
+// <body>
+//     <div class="box">
+//         <img id="imgs" src="logo-350x440 (5).jpg" alt="">
+//         <img id="imgs" src="logo-350x440.jpg" alt="">
+//     </div>
+//     <button id="right">right</button>
+//         <button id="left">left</button>
+// <script>
+//         const rgb = document.getElementById('right')
+//         const lgb = document.getElementById('left')
+//         const imgs = document.getElementById('imgs')
+
+//         let idx = 0;
+
+//         const img = document.querySelectorAll('#imgs imgs')   
+//         function slider (){
+//             imgs.style.transform = `translateX(${-idx*500}px)`;
+//         }
+//         rgb.addEventListener('click',function change(){
+//             idx++;
+//             slider();
+//         })
+    
+//         const image = document.querySelectorAll('#imgs imgs')   
+//         function sliders (){
+//             imgs.style.transform = `translateX(${-idx*-500}px)`;
+//         }
+//         lgb.addEventListener('click',function change(){
+//             idx--;
+//             sliders();
+//         })
+    
+// </script>
+
+// </body>
+// </html>
+
+
+//     // const rgb = document.getElementById('right')
+//     // const lgb = document.getElementById('left')
+//     // const imgs = document.getElementById('imgs')
+
+//     // let idx = 0;
+
+//     // const img = document.querySelectorAll('#imgs imgs')   
+//     // function slider (){
+//     //     imgs.style.transform = `translateX(${-idx*500}px)`;
+//     // }
+//     // rgb.addEventListener('click',function change(){
+//     //     idx++;
+//     //     slider();
+//     // })
+
+
+// // function changeColor (){
+//     //     let currentColor = document.getElementById('demo').style.backgroundColor;
+//     //     if(currentColor == 'red'){
+//     //         document.getElementById('demo').style.backgroundColor = 'blue'
+//     //     }else{
+//     //         document.getElementById("demo").style.backgroundColor = 'red'
+//     //     }
+//     // }
+
+// // function validate(){
+// //     var num=document.myform.num.value;
+// //     if(isNaN(num)){
+// //         document.getElementById("demo").innerHTML = "Enter Numeric value only";
+// //         return false;
+// //     }
+// //     else{
+// //         document.getElementById("demo").innerHTML="";
+// //     }
+// //     }
+
+
+
+
+// BaxtiyorQurbonnazarov
